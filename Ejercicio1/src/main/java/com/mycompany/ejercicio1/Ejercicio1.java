f3e9aa4dd238bd3b20ab38aff4d824ff9ff54acb
pc-9 <pc-9@PC-10>
2025-02-13 14:25:56 +0100

    Mensaje para el segundo ejercicio

--- Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -11,6 +11,6 @@ package com.mycompany.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Archivo cambiado");
     }
 }
